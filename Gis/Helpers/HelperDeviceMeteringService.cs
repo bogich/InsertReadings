12e4a997312a93b1b9f29b7086cb3b47bb85f0d7
bogich <880771@mail.ru>
2018-09-04 12:50:41 +0500

    Добавлена проверка на время выполнения запроса. Если время истекает, то запросу присваивается новый идентификатор во избежание выполнения двух одинаковых запросов.

--- Gis/Helpers/HelperDeviceMeteringService.cs
+++ Gis/Helpers/HelperDeviceMeteringService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ServiceModel;
 using Gis.Crypto;
 using Gis.Infrastructure.DeviceMeteringService;
 using System.Configuration;
@@ -133,6 +134,15 @@ namespace Gis.Helpers.HelperDeviceMeteringService
                     Console.WriteLine(e.Message);
                     Console.ResetColor();
                     Thread.Sleep(1000);
+
+                    if (e.GetType() == typeof(FaultException<Fault>))
+                    {
+                        //Если превышено время обработки запроса, то меняем его идентификатор, что бы не выполнялось два одинаковых запроса одновременно
+                        if (((FaultException<Fault>)e).Detail.ErrorCode.Equals("EXP002002"))
+                        {
+                            reqDeviceMeteringExp.RequestHeader.MessageGUID = Guid.NewGuid().ToString();
+                        }
+                    }
                 }
             }
             while (resDeviceMeteringExp is null);
